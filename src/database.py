9d00ed80eb2f6d647d8cac1ab3172fa3df7fb508
Vlad <vmusaelyae@gmail.com>
2024-11-10 15:40:30 +0200

    added redis connector and redis instance in docker, also mongodb with no connector

--- src/database.py
+++ src/database.py
@@ -1,15 +1,20 @@
-from src.config import DATABASE_URL
+from src.config import DATABASE_URL, REDIS_HOST
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+from redis import Redis
+
+
+redis_conn = Redis(host=REDIS_HOST, port=6379, db=0, decode_responses=True)
 
 async_engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 async_session = sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
 
-# TODO add Redis, MongoDB
+# TODO add MongoDB connector
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
